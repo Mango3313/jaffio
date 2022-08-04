7016cb7b8c1522e32515ece3dbadb236e866bbdb
Mango3313 <daft_noob3313@live.com>
2022-08-04 00:36:33 -0500

    Adding functionality and other pages

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
-$(document).ready(()=>{
-    $("#bodyContainer").load('../pages/about.html');
-});
+window.onload = (e)=>{
+    e.preventDefault();
+    loadPage("about");
+    document.querySelector("#liAbout").addEventListener("click",(e)=>{
+        loadPage("about");
+    });
+    document.querySelector("#liContact").addEventListener("click",(e)=>{
+        loadPage("contact");
+    });
+    document.querySelector("#liProjects").addEventListener("click",(e)=>{
+        loadPage("projects");
+    });
+};
+
+function loadPage(url){
+    window.event.preventDefault();
+
+    fetch("http://localhost:80/jaffio/pages/"+url+".html" /*, options */)
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById("bodyContainer").innerHTML = html;
+    })
+    .catch((error) => {
+        console.warn(error);
+    });
+}
