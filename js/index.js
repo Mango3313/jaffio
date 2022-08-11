f17670e239cd4f584d0fff8d9e78543bf8a595c9
Mango3313 <daft_noob3313@live.com>
2022-08-11 16:49:15 -0500

    Added CV

--- js/index.js
+++ js/index.js
@@ -2,13 +2,22 @@ window.onload = (e)=>{
     e.preventDefault();
     loadPage("about");
     document.querySelector("#liAbout").addEventListener("click",(e)=>{
-        loadPage("about");
+        document.getElementById("bodyContainer").classList.remove("animate__animated");
+        loadPage("about").then(()=>{
+            document.getElementById("bodyContainer").classList.add("animate__animated");
+        });
     });
     document.querySelector("#liContact").addEventListener("click",(e)=>{
-        loadPage("contact");
+        document.getElementById("bodyContainer").classList.remove("animate__animated");
+        loadPage("contact").then(()=>{
+            document.getElementById("bodyContainer").classList.add("animate__animated");
+        });
     });
     document.querySelector("#liProjects").addEventListener("click",(e)=>{
-        loadPage("projects");
+        document.getElementById("bodyContainer").classList.remove("animate__animated");
+        loadPage("projects").then(()=>{
+            document.getElementById("bodyContainer").classList.add("animate__animated");
+        });
     });
 };
 
@@ -18,7 +27,8 @@ function loadPage(url){
     fetch("http://localhost:80/jaffio/pages/"+url+".html" /*, options */)
     .then((response) => response.text())
     .then((html) => {
-        document.getElementById("bodyContainer").innerHTML = html;
+        var body = document.getElementById("bodyContainer");
+        body.innerHTML = html;
     })
     .catch((error) => {
         console.warn(error);
