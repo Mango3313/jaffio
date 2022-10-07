d08510a1ef15b447e4489068ca01c7bd6d14acc2
Mango3313 <daft_noob@live.com>
2022-10-06 20:33:10 -0500

    Completed feature

--- js/index.js
+++ js/index.js
@@ -1,42 +1,52 @@
-window.onload = (e)=>{
+window.onload = (e) => {
     e.preventDefault();
     loadPage("about");
-    document.querySelector("#liAbout").addEventListener("click",(e)=>{
+
+    document.querySelector("#liAbout").addEventListener("click", (e) => {
         document.getElementById("bodyContainer").classList.remove("animate__animated");
         loadPage("about");
     });
-    document.querySelector("#liContact").addEventListener("click",(e)=>{
+    document.querySelector("#liContact").addEventListener("click", (e) => {
         document.getElementById("bodyContainer").classList.remove("animate__animated");
         loadPage("contact");
     });
-    document.querySelector("#liProjects").addEventListener("click",(e)=>{
+    document.querySelector("#liProjects").addEventListener("click", (e) => {
         document.getElementById("bodyContainer").classList.remove("animate__animated");
         loadPage("projects");
     });
-    document.addEventListener('scroll',(e)=>{
+    document.addEventListener('scroll', (e) => {
         var photoDiv = document.querySelector(".header_image");
         var photo = document.querySelector(".header-photo");
-            if(this.scrollY > 300){
-                photoDiv.style.minHeight = 0+"px";
-                photo.style.minHeight = 0+"px";
-            }
-            else{
-                photoDiv.style.minHeight = 300 - this.scrollY+"px";
-                photoDiv.style.transform = "translate3d(0px,"+300 - this.scrollY+"px,0px);"
-            }
+        if (this.scrollY > 300) {
+            photoDiv.style.minHeight = 0 + "px";
+            photo.style.minHeight = 0 + "px";
+        }
+        else {
+            photoDiv.style.minHeight = 300 - this.scrollY + "px";
+            photoDiv.style.transform = "translate3d(0px," + 300 - this.scrollY + "px,0px);"
+        }
     });
 };
 
-function loadPage(url){
+function loadPage(url) {
     window.event.preventDefault();
 
-    fetch("../../jaffio/pages/"+url+".html" /*, options */)
-    .then((response) => response.text())
-    .then((html) => {
-        var body = document.getElementById("bodyContainer");
-        body.innerHTML = html;
-    })
-    .catch((error) => {
-        console.warn(error);
-    });
+    fetch("../../jaffio/pages/" + url + ".html" /*, options */)
+        .then((response) => response.text())
+        .then((html) => {
+            var body = document.getElementById("bodyContainer");
+            body.innerHTML = html;
+        })
+        .catch((error) => {
+            console.warn(error);
+        });
+}
+
+
+function sendEmail() {
+    var field = document.getElementById("inputField").value;
+    var email = document.getElementById("inputEmail").value;
+    var subject = document.getElementById("inputSubject").value;
+    var message = document.getElementById("textAreaMessage").value;
+    window.open('mailto:' + email + '?subject=' + subject + '_from_' + field + '&body=' + message);
 }
